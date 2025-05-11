49db222986aa608edd72d9197e29c04e787c601a
jamesLaurino <petersthomas90@gmail.com>
2025-05-11 15:02:03 +0200

    Ajout de la config rollup pour permettre le await in top projet

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  esbuild: {
+    target: 'esnext', // ou 'es2022'
+  },
+  build: {
+    target: 'esnext', // nécessaire aussi ici
+    rollupOptions: {
+      output: {
+        format: 'es' // important pour autoriser top-level await
+      }
+    }
+  }
 })
